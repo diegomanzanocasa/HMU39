1316af20d2e1db910b4a21dafff6f606dadd5c0a
spardal <samuel.pardal.higuera@tuenti.com>
2020-03-26 16:19:47 +0100

    split work from teams and raspbeddy section

--- teams_pulling.py
+++ teams_pulling.py
@@ -10,14 +10,19 @@ TIME_INTERVAL = 10
 TEAMS_ID = 'c87a997a-cb22-4f4b-b826-d7b84ba19bc3'
 CHANNEL_ID = "19:6330121ab5ae463da28ceedd06897c68@thread.skype"
 
+def raspberry_trigger(text)
+    # todo code to conenct with raspberry
+    pass
+
 
 def check_alarm_messages():
     teams_get_messages_url = 'https://graph.microsoft.com/beta/teams/' + TEAMS_ID +'/' \
                              'channels/' + CHANNEL_ID + '/messages/'
     headers = {"Authorization": 'Bearer ' + access_token_gmc}
-
     try:
         response = requests.get(teams_get_messages_url, headers=headers)
+        # TODO check response here and if it is contains different strings call to raspberry
+        # raspberry_trigger(text)
     except Exception as e:
         with open(FILE_NAME, 'w') as fw:
             fw.write("TEMP")
